17bea4d2b56c18785abb0152fff2cc250ec8689d
ibraKH <ibrakh80@gmail.com>
2022-12-25 18:24:16 +0300

    changing env

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,3 +1,3 @@
-eb init fullstack --platform node.js --region us-east-1 &&
-eb use fullstack-dev
-eb deploy fullstack-dev
+eb init udagram-api-dev --platform node.js --region us-east-1 &&
+eb use udagram-api-dev
+eb deploy udagram-api-dev
