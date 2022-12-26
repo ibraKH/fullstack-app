a6c905d16eebda5a658e8ab37725f26ef0e1c4c7
ibraKH <ibrakh80@gmail.com>
2022-12-26 11:39:16 +0300

    env changes

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,3 +1,3 @@
-eb init udagram-ud --platform node.js --region us-east-1 &&
-eb use udagram-ud && DB_HOST=$DB_HOST DB_USER=$DB_USER DB_PASSWORD=$DB_PASSWORD DB_NAME=$DB_NAME TOKEN_SECRET=$TOKEN_SECRET &&
-eb deploy udagram-ud
+eb init udagram-ud-test --platform node.js --region us-east-1 &&
+eb use udagram-ud-test-dev && DB_HOST=$DB_HOST DB_USER=$DB_USER DB_PASSWORD=$DB_PASSWORD DB_NAME=$DB_NAME TOKEN_SECRET=$TOKEN_SECRET &&
+eb deploy udagram-ud-test-dev
